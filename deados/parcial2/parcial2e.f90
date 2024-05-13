10717f9cf3310e84d1a74f89a1cf916e92c219a4
lucaturi80 <lucaturi80@gmail.com>
2024-05-13 13:30:20 -0300

    luca6

---
+++ deados/parcial2/parcial2e.f90
@@ -0,0 +1,64 @@
+program parcial2e 
+
+use modulos2
+use funciones
+
+implicit none
+
+!defino variables
+real(nr)                            :: r0,r,Vr04,Vr08,Vr0,Er
+integer(ni)                         :: nu1,nu2,n_ite,e,n,i
+real(nr),allocatable,dimension(:)   :: x0,y0
+
+
+!voy a usar el polinomio de lagrange de grado 3 usando los 4 puntos mas cercanos al r que queramos calcular
+
+open(newunit=nu1,file='data/datos.dat',status='old',action='read')
+open(newunit=nu2,file='data/parcial2e.dat',status='replace',action='write')
+
+write(nu2,'(A4,2A18)') '#','r (nm)','V(r) (erg)'
+
+n_ite = 0_ni
+r0 = 0.320_nr
+
+do
+
+ if(r0 >= 0.740_nr) exit
+
+ n_ite = n_ite + 1_ni
+ n = 0_ni
+  
+
+ do
+  read(nu1,*,iostat=e)r     ! esto nos dice ne que pocicion del archivo esta el primer valor mayor que r0
+  if(r >= r0) exit 
+  n = n + 1_ni
+  if(e /= 0) exit
+ end do
+
+ rewind(nu1)
+
+ do i=1,n-3
+  read(nu1,*) r
+ end do
+
+ allocate(x0(0:3),y0(0:3))
+
+ do i=0,3                    ! esto lee los 2 datos antes de r0 y los 2 de despues y los guarda en un
+    read(nu1,*) x0(i),y0(i)  ! en 2 vectores para darle al modulo de interpolacion de lagrange
+ end do
+
+ call lagrange(3,x0,y0,r0,Vr04)
+ print*, Vr04
+
+ r0 = r0 + 0.005_nr
+
+ write(nu2,'(I4,2F18.8)') n_ite,r,Vr04
+ 
+
+ rewind(nu1)
+ deallocate(x0,y0)
+end do
+
+
+end program parcial2e
---
+++ deados/parcial2/runners/run_e.sh
@@ -0,0 +1,2 @@
+gfortran parcial2e.f90 precision/precision_global.f90 precision/precision.f90 modulos/modulos_interpolacion.f90 modulos/funciones.f90 -o parcial2e.out -J modulos_mod
+./parcial2e.out
