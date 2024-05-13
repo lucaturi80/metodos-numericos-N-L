140b3d37ebc40a43ec207fd4825927560b8e13ec
nachoagost <anacho595@gmail.com>
2024-05-13 10:31:15 -0300

    masomenos el c,y  funciones de fortran

--- deados/parcial2/parcial2c.f90
+++ deados/parcial2/parcial2c.f90
@@ -13,23 +13,33 @@ INTEGER(KIND = ni) i
 REAL(KIND = nr) rmin, rmax, rvar
 REAL(KIND = nr) dfx, EAdf
 
-
+open(unit = 10, file = "data/parcial2c.dat")
+write(10,*) "r, dv/dr ( r), EAX comparandolo con la derivada analiticamente"
 !----------------------------------
 
 !----------------------------------
-!proceso de datos
+!proceso e impresion de datos
 !----------------------------------
+
 rmin = 0.36_nr!nm
 rmax = 0.75_nr!nm
 
 
 rvar = rmin
-do 
-dfx = 
-write(10,66) 
+DO 
+    IF (rvar = rmin) THEN
+        dfx = dF2I(0.005_nr, fp(rvar), fp(rvar+0.005_nr))
+    ELSE IF (rvar = rmax) THEN
+        dfx = dF2D(0.005_nr, fp(rvar-0.005_nr), fp(rvar))
+    ELSE 
+        dfx = dF3C(0.005_nr, fp(rvar-0.005), fp(rvar), fp(rvar+0.005))
+    END IF
+    EAdf = abs( dfp(rvar) - dfx )
+    write(10,66) rvar, dfx, EAX
+    if (rvar = rmax) exit
 
-end do
+END DO
 
 !----------------------------------
-
+close(10)
 end program
